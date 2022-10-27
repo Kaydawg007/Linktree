6914c1dc3479bceb9e3f28423d7ccb37c5e28985
Kayode Uthman <88583385+Kaydawg007@users.noreply.github.com>
2022-10-27 17:51:20 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Main/Header";
+import Links from "./components/Main/Links";
+import Footer from "./components/Main/Footer";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Links />
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+export default App;
